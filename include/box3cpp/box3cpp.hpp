7dbbc69eb6461d87e650a64de2c5445507feb238
verte1 <174943900+kio-png@users.noreply.github.com>
2026-07-08 11:20:01 +0300

    Set up initial CMake skeleton

---
+++ include/box3cpp/box3cpp.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include <box3cpp/version.hpp>
---
+++ include/box3cpp/version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace box3cpp {
+
+inline constexpr int version_major = 0;
+inline constexpr int version_minor = 1;
+inline constexpr int version_patch = 0;
+
+inline constexpr const char* version_string = "0.1.0";
+
+} // namespace box3cpp
